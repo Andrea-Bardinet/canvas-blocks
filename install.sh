7d1b992dfb0dfb22ebdddf28d7e9a9ad903c19c3
Andrea <bardinet.andrea@gmail.com>
2024-01-05 07:33:42 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 npm install
 npm run build
 
-docker container stop nginx
-docker container stop traefik
-docker container remove nginx
-docker container remove traefik
-
-docker-compose up -d  
---
+++ run.sh
@@ -0,0 +1,6 @@
+docker container stop nginx
+docker container stop traefik
+docker container remove nginx
+docker container remove traefik
+
+docker-compose up -d  
