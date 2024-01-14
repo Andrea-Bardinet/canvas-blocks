82937c08987584fdd3aceb1bc3e96d0ea087cdf0
Andréa Bardinet <34200226+Andrea-Bardinet@users.noreply.github.com>
2024-01-14 22:58:35 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-docker container stop canvas-blocks && docker container remove canvas-blocks
-docker container stop portfolio && docker container remove portfolio
-docker container stop traefik && docker container remove traefik
-
-docker-compose up -d
